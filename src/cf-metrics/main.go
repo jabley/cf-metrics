7a8da12bc25822ac0bf5df1cae7a6dcf69cdd337
James Abley <james.abley@gmail.com>
2016-10-23 10:58:30 +0100

    Sketch out retrieving zone info

--- src/cf-metrics/main.go
+++ src/cf-metrics/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
-	"sync"
+	"strings"
 	"time"
+	// "github.com/cloudfoundry/cli/cf/configuration/coreconfig"
 )
 
 type CFInfo struct {
 	API      string
 	username string
 	password string
+}
+
+// type Zone struct {
+// 	config        coreconfig.ReadWriter
+// 	authenticator authentication.Repository
+// 	endpointRepo  coreconfig.EndpointRepository
+// 	orgRepo       organizations.OrganizationRepository
+// 	spaceRepo     spaces.SpaceRepository
+// }
+
+type AppMetrics struct {
 }
 
 func main() {
@@ -21,14 +34,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go workLoop(zones)
-	wg.Wait()
+	encoder := json.NewEncoder(os.Stdout)
+
+	logMetric := func(metrics AppMetrics) {
+		if err := encoder.Encode(metrics); err != nil {
+			panic(err)
+		}
+	}
+
+	metrics := make(chan AppMetrics)
+
+	// wg := &sync.WaitGroup{}
+	// wg.Add(1)
+	go workLoop(zones, metrics)
+
+	for m := range metrics {
+		logMetric(m)
+	}
+	// wg.Wait()
 }
 
-func workLoop(zones []CFInfo) {
-	t := time.NewTicker(time.Duration(5) * time.Second)
+func workLoop(zones []CFInfo, metrics chan AppMetrics) {
+	t := time.NewTicker(time.Duration(10) * time.Second)
 
 	for {
 		select {
@@ -45,6 +72,18 @@ func workLoop(zones []CFInfo) {
 func parseZones() []CFInfo {
 	zones := make([]CFInfo, 0)
 
+	zonePrefixes := strings.Split(getDefaultConfig("ZONE_PREFIXES", ""), ",")
+
+	for _, prefix := range zonePrefixes {
+		if len(prefix) == 0 {
+			continue
+		}
+		username := getDefaultConfig(prefix+"_USERNAME", "")
+		password := getDefaultConfig(prefix+"_PASSWORD", "")
+		api := getDefaultConfig(prefix+"_API", "")
+
+		zones = append(zones, CFInfo{API: api, username: username, password: password})
+	}
 	return zones
 }
 
