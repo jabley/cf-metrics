b513835ed3cde64dfc4cb5e81750851a62f9644a
James Abley <james.abley@gmail.com>
2016-11-10 12:18:23 +0000

    Documentation to make goreportcard.com happy

--- src/cf-metrics/events.go
+++ src/cf-metrics/events.go
@@ -12,17 +12,23 @@ import (
 	"code.cloudfoundry.org/cli/cf/net"
 )
 
+// EventRepo provides functionality to query a Cloud Foundry API for events
+// related to an application.
 type EventRepo struct {
 	config  coreconfig.Reader
 	gateway net.Gateway
 }
 
+// NewEventRepo returns a new EventRepo which internally uses the specified
+// coreconfig.Repository and net.Gateway.
 func NewEventRepo(config coreconfig.Repository, gateway net.Gateway) (repo EventRepo) {
 	repo.config = config
 	repo.gateway = gateway
 	return
 }
 
+// GetAppEvents fetches all events for the specified app that have happened
+// since the specified time. For each event, it calls the provided callback function.
 func (r EventRepo) GetAppEvents(app models.Application, since time.Time, callback func(models.EventFields) bool) error {
 	return r.gateway.ListPaginatedResources(
 		r.config.APIEndpoint(),
